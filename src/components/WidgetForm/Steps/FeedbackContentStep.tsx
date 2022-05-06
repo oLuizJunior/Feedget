c1864c9fc8753da24595c6f15d01161fc124018a
Luiz Rodrigues Junior <junior.luiz182@gmail.com>
2022-05-06 12:49:43 -0300

    finalizando o formulario Widget

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,15 +1,37 @@
 
-import { ArrowLeft, Camera } from "phosphor-react";
+import { FormEvent, useState } from "react";
+import { ArrowLeft} from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton"
+
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType,
   onFeedbackRestartRequested: () => void,
+  onFeedbackSent: () => void,
 }
 
-export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: FeedbackContentStepProps){
+export function FeedbackContentStep({
+  feedbackType, 
+  onFeedbackRestartRequested,
+  onFeedbackSent
+}: FeedbackContentStepProps){
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('');
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent){
+    event.preventDefault();
+
+    console.log({
+      screenshot, 
+      comment,
+    })
+
+    onFeedbackSent();
+  }
 
     return( 
       <>
@@ -22,7 +44,6 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: 
             <ArrowLeft weight="bold" className="w-4 h-4"/>
           </button>
 
-
           <span className="text-xl leading-6 flex items-center gap-2">
             <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
             {feedbackTypeInfo.title}
@@ -31,25 +52,24 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: 
           <CloseButton />
         </header>
   
-       <form className="my-4 w-full">
+       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea 
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo"
+          onChange={event => setComment(event.target.value)}
         />
 
-
         <footer className="flex gap-2 mt-2">
-          <button 
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" alt="Botão com o icone de uma câmera que tira print da tela."/>
-          </button>
 
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
 
           <button
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            disabled={comment.length === 0}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500 "
           >
             Enviar Feedback
           </button>
